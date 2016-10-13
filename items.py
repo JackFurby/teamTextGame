5c5f1bef3f023ed80b58a81b6978720039577533
JackFurby <jackfurby@live.co.uk>
2016-10-13 13:32:14 +0100

    added items
    add items here

---
+++ items.py
@@ -0,0 +1,8 @@
+item_phone = {
+    "id": "phone",
+
+    "name": "Samsung Galaxy Note 7",
+
+    "description":
+    """You have 15% battery left. lets hope it does not explode when it is charged..."""
+}
