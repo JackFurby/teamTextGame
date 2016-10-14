ff986e8002237ec81bd6edc58dd27c34e9d16148
JaviAZ <javialcazafra@gmail.com>
2016-10-14 14:36:31 +0100

    Updated map.

--- map.py
+++ map.py
@@ -1,63 +1,97 @@
-from items import *
-
-room_Office1 = {
+room_Office1={
 	"name":"Office 1",
 	"description":
     """Your office is small with a chair by your desk and a bench against the wall. There is a door to the east leading to Therapy""",
 	"items":[],
-	"exits":{"East":"Therapy room"},
-	"map": ["------------", "|          |", "| OFFICE 1 |", "|          |", "|          |", "---- \------"]
+	"exits":{"East":"Room 483"}
 }
-room_Office2 = {
+room_Office2={
 	"name":"Office 2",
 	"description":
     """add me""",
 	"items":[],
-	"exits":{"north":"Canteen","south":"Room 666"}	
+	"exits":{"north":"Emergency room","south":"Canteen","east":"Yard","west":"Therapy room"}	
 }
-room_Reception = {
+room_Reception={
 	"name":"Reception",
 	"description":
     """add me""",
 	"items":[],
-	"exits":{"east":"The yard","west":"Emergency room"}	
+	"exits":{"south":"Room 123","west":"Emergency room"}	
 }
-room_Therapy = {
+room_Therapy={
 	"name":"Therapy room",
 	"description":
     """add me""",
 	"items":[],
-	"exits":{"west":"Office 1","east":"Canteen"}	
+	"exits":{"east":"Office 2","south":"Room 483"}	
 }
-room_Canteen = {
+room_Canteen={
 	"name":"Canteen",
 	"description":
     """add me""",
 	"items":[],
-	"exits":{"west":"Therapy room","south":"Office 2","east":"Emergency room"}	
+	"exits":{"west":"Room 483","south":"Room 234","east":"Room 251","north":"Office 2"}	
 }
-room_666 = {
+room_666={
 	"name":"Room 666",
 	"description":
     """add me""",
 	"items":[],
-	"exits":{"north":"Office 2"}	
+	"exits":{"north":"Room 234"}	
 }
-room_ER = {
+room_ER={
 	"name":"Emergency room",
 	"description":
     """add me""",
 	"items":[],
-	"exits":{"west":"Canteen","east":"Reception"}
+	"exits":{"south":"Office 2"}
 }
-room_Yard = {
+room_Yard={
 	"name":"The yard",
 	"description":
     """add me""",
 	"items":[],
-	"exits":{"north":"Reception"}
+	"exits":{"west":"Office 2","south":"Room 251"}
 }
-rooms = {
+room_483={
+	"name":"Room 483",
+	"description":
+    """add me""",
+	"items":[],
+	"exits":{"north":"Therapy room","west":"Office 1","south":"Room 645","east":"Canteen"}	
+}room_645={
+	"name":"Room 645",
+	"description":
+    """add me""",
+	"items":[],
+	"exits":{"north":"Room 483","east":"Room 234"}	
+}room_234={
+	"name":"Room 234",
+	"description":
+    """add me""",
+	"items":[],
+	"exits":{"north":"Canteen","south":"Room 666","east":"Room 347","west":"645"}	
+}room_123={
+	"name":"Room 123",
+	"description":
+    """add me""",
+	"items":[],
+	"exits":{"north":"Reception"}	
+}room_251={
+	"name":"Room 251",
+	"description":
+    """add me""",
+	"items":[],
+	"exits":{"north":"Yard","south":"Room 347","east":"Reception","west":"Canteen"}	
+}room_347={
+	"name":"Room 347",
+	"description":
+    """add me""",
+	"items":[],
+	"exits":{"north":"Room 251","east":"Room 123","west":"234"}	
+}
+rooms={
 	"Office 1":room_Office1,
 	"Office 2":room_Office2,
 	"Reception":room_Reception,
@@ -66,4 +100,10 @@ rooms = {
 	"Room 666":room_666,
 	"Emergency room":room_ER,
 	"The yard":room_Yard
+	"Room 483":room_483,
+	"Room 645":room_645,
+	"Room 234":room_234,
+	"Room 123":room_123,
+	"Room 347":room_347,
+	"Room 251":room_251
 }
