0d8d665ec2a2743a4c10f12a10437eb5f57e6d4b
JaviAZ <javialcazafra@gmail.com>
2016-10-14 14:59:39 +0100

    Renamed from Player.py to Character.py
    Renamed from Player.py to Character.py
    Added Player and Cannibal
    Created dictionary of players

---
+++ Characters.py
@@ -0,0 +1,25 @@
+from items import *
+from map import rooms
+
+Player={
+  # Start game at the reception
+  "name":"Doc",
+  "current_room":rooms["Office 1"],
+  "inventory":[]
+}
+Cannibal=[
+  "name":"Hannibal the cannibal",
+  "current_room":rooms["Room 666"],
+  "inventory":[],
+  "file":""
+]
+Players={
+  Player:"",
+  Cannibal:"",
+  Patient_123:"",
+  Patient_483:"",
+  Patient_645:"",
+  Patient_234:"",
+  Patient_251:"",
+  Patient_347:""
+}
--- player.py
+++
@@ -1,7 +0,0 @@
-from items import *
-from map import rooms
-
-inventory = []
-
-# Start game at the reception
-current_room = rooms["Office 1"]
