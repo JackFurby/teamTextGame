e90f819cf9f23fc55b368c56557ae04a89772481
Ermal Laci <laciermal98@gmail.com>
2016-10-18 13:28:56 +0100

    commented combat.py

--- combat.py
+++ combat.py
@@ -1,6 +1,7 @@
 from characters import Player, Cannibal
 from random import randint
 
+#Create a dictionary with stats used for the player in the fight
 player_fighting = {
 	"health": 100,
 	"strength": 30,
@@ -8,6 +9,7 @@ player_fighting = {
 	"alive": True
 }
 
+#Create a dictionary with stats used for the cannibal in the fight
 cannibal_fighting = {
 	"health": 100,
 	"strength": 30,
@@ -22,45 +24,56 @@ def player_attack():
 		print("1. POWER Strike")
 		print("2. Precision Strike")
 
+		#Get the players input for either power or precision strike
 		type_of_attack = input("> ")
 
 		if type_of_attack == "1":
 
+			#Generate a random number between 0 and the players strength
 			dmg_dealt = randint(0, player_fighting["strength"])
-			print(player_fighting["strength"])
-			print(dmg_dealt)
+
+			#checks if any damage is dealt
+			#If damage is 0 the player missed
 			if dmg_dealt == 0:
 				return "\nYou missed."
 
+			#If the damage is less than the cannibals defence the attack is blocked
 			elif dmg_dealt < cannibal_fighting["defence"]:
 				return "\nYour attack was blocked."
 
+			#Otherwise deal the damage and take away the damage dealt calculation from the cannibals health
 			else:
 				cannibal_fighting["health"] -= dmg_dealt
 				return "\nYou deal " + str(dmg_dealt) + " damage to " + Cannibal["name"] + "."
 
+		#if its a precision strike the attack will always hit
 		elif type_of_attack == "2":
 
+			#The attack has a much lower maximum damage, calculate this value and take it away from the cannibals health
 			dmg_dealt = randint(0, player_fighting["strength"] - 15)
 			cannibal_fighting["health"] -= dmg_dealt
 
 			return "\nYou deal " + str(dmg_dealt) + " damage to " + Cannibal["name"] + "."
 
+		#if the player doesn't input 1 or 2 then loop until there is a valid input
 		else:
 			print("\nPlease choose option 1 or 2")
 
 def cannibal_attack():
 
+	#If the cannibal hasn't evolved yet the give a 1 in 10 chance to evolve
 	if cannibal_fighting["evolved"] == False:
 		evolve = randint(1, 10)
 		if evolve == 1:
+			#Set the evolved value to true and double the cannibal's strength
 			cannibal_fighting["evolved"] == True
 			cannibal_fighting["strength"] = cannibal_fighting["strength"] * 2
 			return "\nWhat?\n" + Cannibal["name"] + " is evolving!"
 
-	
+	#Generate a random chance for either a bite or scratch
 	type_of_attack = randint(0, 1)
 
+	#If the number generated is 0 then the cannibal bites, this is the equivalent of a power strike.
 	if type_of_attack == 0:
 
 		print("\n" + Cannibal["name"] + " uses bite")
@@ -77,6 +90,7 @@ def cannibal_attack():
 			player_fighting["health"] -= dmg_dealt
 			return "\n" + Cannibal["name"] + " deals " + str(dmg_dealt) + " damage to you."
 	
+	#If the number generated is 0 then the cannival scratches which is the equivalent of a precision strike.
 	elif type_of_attack == 1:
 
 		print("\n" + Cannibal["name"] + " uses scratch")
@@ -92,6 +106,7 @@ def fight_main():
 	print("It's time to FIGHT FOR YOUR LIFE!")
 	print("Before the battle begins you have to remember, are you RIGHT or LEFT handed?")
 
+	#loop until the user inputs either right or left and change the strength or defence appropriately
 	while True:
 		user_input = input("> ")
 		user_input = user_input.lower()
@@ -108,12 +123,14 @@ def fight_main():
 	print("\n" + Player["name"] + "     VS     " + Cannibal["name"] + "\n")
 
 
+	#Loop the fight until someone dies
 	while cannibal_fighting["alive"] == True and player_fighting["alive"] == True:
 		print("\n" + Player["name"] + " HP: " + str(player_fighting["health"]) + "\n")
 		print("\n" + Cannibal["name"] + " HP: " + str(cannibal_fighting["health"]) + "\n")
 
 		print(player_attack())
 
+		#Checks if the cannibal survived and if he did then the cannibal attacks
 		if cannibal_fighting["health"] > 1:
 			print(cannibal_attack())
 			if player_fighting["health"] < 1:
